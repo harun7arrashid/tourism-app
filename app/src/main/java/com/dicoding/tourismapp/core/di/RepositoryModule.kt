71de0802f558e42d778a8b359ce1f02b719c49fc
harun7arrashid <harun4arrashid@gmail.com>
2021-10-31 14:40:18 +0700

    Dagger Hilt Finish

--- app/src/main/java/com/dicoding/tourismapp/core/di/RepositoryModule.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/RepositoryModule.kt
@@ -4,11 +4,14 @@ import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
 @Module(includes = [NetworkModule::class, DatabaseModule::class])
+@InstallIn(SingletonComponent::class) // Module menggunakan Component yg mana, Di sini RepositoryModule masuk ke SingletonComponent karena ia merupakan Component dengan hirarki tertinggi yang bisa digunakan di mana saja.
 abstract class RepositoryModule {
 
-    @Binds // knp make @binds, soalnya ini hrs mengimplement konkret class dari si interface
+    @Binds
     abstract fun provideRepository(tourismRepository: TourismRepository): ITourismRepository
 
 }
